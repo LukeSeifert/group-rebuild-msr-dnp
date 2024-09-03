29068d1b4407851ea714719336af005b6a356e82
Luke Seifert <LukeSeifert23@gmail.com>
2024-09-03 14:20:34 -0500

    Add nuclide comparison and doc strings

--- scripts/irradiation/runner.py
+++ scripts/irradiation/runner.py
@@ -1,5 +1,7 @@
 from simple import IrradSimple
 import copy
+import matplotlib.pyplot as plt
+import os
 
 class Run:
     """
@@ -7,21 +9,36 @@ class Run:
 
     """
 
-    def __init__(self, run_omc=False, analyze=False):
+    def __init__(self, nuc_list: list, run_omc: bool = False, analyze: bool = False):
+        """
+
+        Parameters
+        ----------
+        nuc_list : list
+            List of nuclides to analyze
+        run_omc : bool, optional
+            Run OpenMC irradiation simulation, by default False
+        analyze : bool, optional
+            Run analysis of results, by default False
+        """
         self.run_omc = run_omc
         self.analyze = analyze
-        self.metadata = dict()
-        self.metadata['conc'] = dict()
-        self.metadata['fiss'] = dict()
+        self.nuc_list = nuc_list
+        self.metadict = dict()
+        self.metadict['conc'] = dict()
+        self.metadict['fiss'] = dict()
+        self.metadict['times'] = dict()
         return
     
-    def _simple_run(self, simple_irrad_obj, version):
+    def _simple_run(self, simple_irrad_obj):
         if self.run_omc:
             simple_irrad_obj.irradiate()
-        concs = simple_irrad_obj.collect_concentrations()
+        concs, times = simple_irrad_obj.collect_concentrations()
         fisses = simple_irrad_obj.collect_fissions()
-        self.metadata['conc'][version] = concs
-        self.metadata['fiss'][version] = fisses
+        version = simple_irrad_obj.name
+        self.metadict['conc'][version] = concs
+        self.metadict['fiss'][version] = fisses
+        self.metadict['times'][version] = times
         return
 
     
@@ -31,18 +48,42 @@ class Run:
 
         """
         for simple_irrad_obj in args:
-            self._simple_run(simple_irrad_obj, version='flowing')
-        
+            self._simple_run(simple_irrad_obj) 
+
         if self.analyze:
-            pass
+            self._nuc_compare()
+
+
         return
+    
+
+    def _nuc_compare(self):
+        for nuc in self.nuc_list:
+            for version in self.metadict['conc'].keys():
+                conc = self.metadict['conc'][version][nuc]
+                times = self.metadict['times'][version]
+                plt.plot(times, conc, label=version)
+            plt.xlabel('Time [s]')
+            plt.ylabel('Concentration [atoms]')
+            plt.legend()
+            plt.tight_layout()
+            try:
+                plt.savefig(f'./images/{nuc}_conc.png')
+            except FileNotFoundError:
+                os.mkdir('./images')
+                plt.savefig(f'./images/{nuc}_conc.png')
+            plt.close()
+        return
+
 
 
 
 
 if __name__ == "__main__":
     import ui
-    runner = Run(True, False)
+    runner = Run(ui.nuc_list,
+                 run_omc=False,
+                 analyze=True)
     flowing = IrradSimple(data_dict=ui.base_case_data)
     static = IrradSimple(data_dict=ui.static_data)
     runner.simple_compare(flowing,
