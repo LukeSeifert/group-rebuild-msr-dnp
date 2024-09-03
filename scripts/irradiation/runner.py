570442c9dde7f932b6e1fdfdb4c397fc7520c6be
Luke Seifert <LukeSeifert23@gmail.com>
2024-09-03 15:33:44 -0500

    Add fission analysis

--- scripts/irradiation/runner.py
+++ scripts/irradiation/runner.py
@@ -2,6 +2,8 @@ from simple import IrradSimple
 import copy
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+import scipy as sp
 
 class Run:
     """
@@ -40,6 +42,22 @@ class Run:
         self.metadict['fiss'][version] = fisses
         self.metadict['times'][version] = times
         return
+    
+    def _fission_analysis(self):
+        for vi, version in enumerate(self.metadict['fiss'].keys()):
+            fission_rates = self.metadict['fiss'][version]['net']
+            times = self.metadict['times'][version]
+            fissions = sp.integrate.simpson(fission_rates, x=times)
+            target_nuc = self.irrad_objs[vi].sample_nuc
+            target_fiss_rate = self.metadict['fiss'][version][target_nuc]
+            target_fiss = sp.integrate.simpson(target_fiss_rate, x=times)
+            fiss_frac = target_fiss / fissions
+            net_fiss = np.sum(fissions)
+            avg_fiss_rate = net_fiss / times[-1]
+            print(version)
+            print(f'     {avg_fiss_rate=:.3E}')
+            print(f'     {net_fiss=:.3E}')
+        return
 
     
     def simple_compare(self, *args):
@@ -47,10 +65,13 @@ class Run:
         Takes in any number of `IrradSimple` objects to be compared
 
         """
+        self.irrad_objs = list()
         for simple_irrad_obj in args:
             self._simple_run(simple_irrad_obj) 
+            self.irrad_objs.append(simple_irrad_obj)
 
         if self.analyze:
+            self._fission_analysis()
             self._nuc_compare()
 
 
@@ -82,7 +103,7 @@ class Run:
 if __name__ == "__main__":
     import ui
     runner = Run(ui.nuc_list,
-                 run_omc=False,
+                 run_omc=True,
                  analyze=True)
     flowing = IrradSimple(data_dict=ui.base_case_data)
     static = IrradSimple(data_dict=ui.static_data)
