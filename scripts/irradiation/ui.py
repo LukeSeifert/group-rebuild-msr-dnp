ce89c27653c1706dfdae8b313bb1115bdaa60b77
Luke Seifert <LukeSeifert23@gmail.com>
2024-09-04 07:44:01 -0500

    Added dictionary builder to ui

--- scripts/irradiation/ui.py
+++ scripts/irradiation/ui.py
@@ -2,42 +2,50 @@ import matplotlib.pyplot as plt
 
 nuc_list = ['Xe135']
 
-name = 'flowing'
-base_case_data = {
-'name': name,
-'t_incore_s': 10,
-'t_excore_s': 10,
-'n_MeV': 0.0253 * 1e-6,
-'S_rate_per_s': 1e10,
-'batches': 10,
-'output_path': f'./results/{name}',
-'nps': 1000,
-'photon_bool': False,
-'run_mode': 'fixed source',
-'temperature_K': 298.15,
-'fissile_nuc': 'U235',
-'dens_g_cc': 10,
-'chain': '../../data/chain/chain_casl_pwr.xml'
-}
+default_batches = 100
+default_nps = 1000
+default_source = 1e10
+default_temperature = 298.15
+default_energy = 0.0253 * 1e-6
+default_photons = False
+default_run_mode = 'fixed source'
+default_fissile = 'U235'
+default_dens = 10
+default_chain = '../../data/chain/chain_casl_pwr.xml'
+default_incore_t = 4
+default_excore_t = 4
+default_name = 'default'
+default_path = f'./results/{default_name}'
 
-name = 'static'
-static_data = {
-'name': name,
-'t_incore_s': 20,
-'t_excore_s': 0,
-'n_MeV': 0.0253 * 1e-6,
-'S_rate_per_s': 1e10/2,
-'batches': 10,
-'output_path': f'./results/{name}',
-'nps': 1000,
-'photon_bool': False,
-'run_mode': 'fixed source',
-'temperature_K': 298.15,
-'fissile_nuc': 'U235',
-'dens_g_cc': 10,
-'chain': '../../data/chain/chain_casl_pwr.xml'
-}
 
+def dict_builder(name=default_name, incore_t=default_incore_t,
+                 excore_t=default_excore_t, n_MeV=default_energy,
+                 S_rate_per_s=default_source, batches=default_batches,
+                 nps=default_nps, photon_bool=default_photons, 
+                 run_mode=default_run_mode, temperature_K=default_temperature,
+                 fissile_nuc=default_fissile, dens_g_cc=default_dens,
+                 chain=default_chain):
+    data = {
+    'name': name,
+    't_incore_s': incore_t,
+    't_excore_s': excore_t,
+    'n_MeV': n_MeV,
+    'S_rate_per_s': S_rate_per_s,
+    'batches': batches,
+    'output_path': f'./results/{name}',
+    'nps': nps,
+    'photon_bool': photon_bool,
+    'run_mode': run_mode,
+    'temperature_K': temperature_K,
+    'fissile_nuc': fissile_nuc,
+    'dens_g_cc': dens_g_cc,
+    'chain': chain
+    }
+    return data
+
+base_case_data = dict_builder(name='flowing', incore_t=10, excore_t=10)
+static_data = dict_builder(name='static', incore_t=20, excore_t=0,
+                           S_rate_per_s=default_source/2)
 
 
 
