9b9b07b8ae41d94164e4dd905f81fd946a89bb10
Luke Seifert <LukeSeifert23@gmail.com>
2024-09-12 17:01:49 -0500

    Automatically update source rate

--- scripts/ui.py
+++ scripts/ui.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-nuc_list = ['Xe135', 'Br87', 'Kr100', 'I137', 'Cs141']
+nuc_list = ['Rb91', 'Br87', 'Cs141']
 
 default_batches = 10
 default_nps = 100
-default_source = 1e14
+default_source = 4.2e16
 default_temperature = 298.15
 default_energy = 0.0253 * 1e-6
 default_photons = False
@@ -15,7 +15,7 @@ default_incore_t = 4
 default_excore_t = 4
 default_name = 'Default'
 default_path = f'./results/{default_name}'
-default_final_time = 5 * 60
+default_final_time = 7 * 60
 
 if default_energy < 300 * 1e-6:
     default_chain = '../data/chain/chain_endfb80_pwr.xml'
@@ -27,11 +27,12 @@ else:
 
 def dict_builder(name=default_name, incore_t=default_incore_t,
                  excore_t=default_excore_t, n_MeV=default_energy,
-                 S_rate_per_s=default_source, batches=default_batches,
+                 net_Sp=default_source, batches=default_batches,
                  nps=default_nps, photon_bool=default_photons, 
                  run_mode=default_run_mode, temperature_K=default_temperature,
                  fissile_nuc=default_fissile, dens_g_cc=default_dens,
                  chain=default_chain, final_time=default_final_time):
+    S_rate_per_s = net_Sp * (excore_t + incore_t) / (incore_t * final_time)
     data = {
     'name': name,
     't_incore_s': incore_t,
@@ -51,11 +52,16 @@ def dict_builder(name=default_name, incore_t=default_incore_t,
     }
     return data
 
-base_case_data = dict_builder(name='Flowing', incore_t=10, excore_t=10)
-static_data = dict_builder(name='Static', incore_t=10*2, excore_t=0,
-                           S_rate_per_s=default_source/2)
-pulse_data = dict_builder(name='Pulse', incore_t=10/10000, excore_t=0,
-                          final_time=10/10000, S_rate_per_s=default_source*10000)
+flow_data = dict_builder(name='Flowing', incore_t=10, excore_t=10)
+
+static_data = dict_builder(name='Static', incore_t=10*2, excore_t=0)
+
+pulse_data = dict_builder(name='Pulse', incore_t=1/4*1e-3, excore_t=0,
+                          final_time=10/4*1e-3)
+
+mostly_excore_data = dict_builder(name='ExFlowing', incore_t=10/10,
+                                  excore_t=10)
+
 
 
 
